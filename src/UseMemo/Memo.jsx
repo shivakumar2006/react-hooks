0c8e1eb91aa58f0ca0d0b40d2c10bc3600170bb2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-03-21 21:12:28 +0530

    commit

--- src/UseMemo/Memo.jsx
+++ src/UseMemo/Memo.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 
 const Memo = () => {
+  const [count, setCount] = useState(0);
+
+  const expensiveCalculations = useMemo(() => {
+    console.log("Calculating sum...");
+    let sum = 0;
+    for (let i = 0; i <= 1000000; i++) {
+      sum += i;
+      console.log("value of i : ", i);
+    }
+    return sum;
+  }, []); // Cached on initial render
+
   return (
-    <div>Memo</div>
+    <div>
+      <p>Expensive calculation result: {expensiveCalculations}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <p>Count: {count}</p>
+    </div>
   )
 }
 
-export default Memo
+export default Memo;
